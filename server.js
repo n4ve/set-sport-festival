6b8caecfab8b802bf10688ffd635dbd960b69315
Navee Sratthatad <navees@set.or.th>
2019-05-18 15:32:52 +0700

    Add Upload Page

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Fast html framework
 var express = require('express');
 var app = express();
+var multer  = require('multer')
  
 // For receiving JSON in posts
 var bodyParser = require('body-parser');
@@ -10,11 +11,17 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 // For the database
 var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database('./db/set-festival.db');
+var csv = require('fast-csv');
+
+var upload = multer({ dest: 'uploads/' })
  
 // Add restful controller
 require('./ScoreController')(app, db, jsonParser);
+require('./UploadScoreController')(app, db, csv, upload);
  
 // Serve static files
 app.use(express.static('wwwroot'))
+
+
  
 app.listen(3000);
