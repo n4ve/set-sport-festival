257cfde8297eafc960482ba2fbe544d2da9d29f3
Navee Sratthatad <navees@set.or.th>
2019-05-19 09:01:39 +0700

    Fix git ignore + reconstruct path

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var multer  = require('multer')
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json()
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+const port = process.env.PORT || 5000;
  
 // For the database
 var sqlite3 = require('sqlite3');
@@ -14,15 +16,26 @@ var db = new sqlite3.Database('./db/set-festival.db');
 var csv = require('fast-csv');
 
 var upload = multer({ dest: 'uploads/' })
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
  
 // Add restful controller
 require('./ScoreController')(app, db, jsonParser);
 require('./UploadScoreController')(app, db, csv, upload);
- 
-app.get('/', function (req, res) {
-    res.send('Hellow');
+
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
 });
 
 
-console.log('Listening Port ' + process.env.PORT || 3000);
-app.listen(process.env.PORT || 3000);
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}/`);
+  });
+
+
